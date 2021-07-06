9aa2a4a169a28e73ff29bbe5c65851cc100d8c08
Malwande <malwande129@gmail.com>
2021-07-06 21:05:20 +0200

    Registered the posts app in Settings
    Registered the posts app in Settings
    
    Registered the posts app in Settings

--- blog/settings.py
+++ blog/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #registered the posts app installed apps
+    'posts',
 ]
 
 MIDDLEWARE = [
