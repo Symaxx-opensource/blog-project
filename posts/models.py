f5784f0d21f23afeac9f94de1d6f18c20ab7913d
Bukhosi Moyo <61363899+BukhosiMoyo@users.noreply.github.com>
2021-07-06 17:24:23 +0200

    Added some spaces for readability

--- posts/models.py
+++ posts/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django import forms
 import datetime
 
 # Create your models here.
 class UserProfile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
@@ -19,7 +19,7 @@ class Post(models.Model):
     post_description = models.TextField(max_length=4000)
     published_post = models.BooleanField(default=False)
     published_date = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(UserProfile,on_delete=models.CASCADE,related_name='author')
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE,related_name='author')
     created_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
@@ -30,12 +30,12 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
     def get_approved_comments(self):
-        return self.comments.filter(approved_comment =True)
+        return self.comments.filter(approved_comment = True)
     def get_absolute_url(self):
         return reverse('index')
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post,on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     comment = models.CharField(max_length=500)
     author_name = models.CharField(max_length=100)
     approved_comment = models.BooleanField(default=False)
